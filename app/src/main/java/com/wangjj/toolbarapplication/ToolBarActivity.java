ae86665c5299a893dbba077ae93453b7e15b8e79
wangjianjun <wangjianjun>
2016-03-14 17:51:20 +0800

    。

--- app/src/main/java/com/wangjj/toolbarapplication/ToolBarActivity.java
+++ app/src/main/java/com/wangjj/toolbarapplication/ToolBarActivity.java
@@ -1,14 +1,19 @@
 package com.wangjj.toolbarapplication;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 /**
  * Created by wangjj on 2016/3/11.
  */
 public class ToolBarActivity extends AppCompatActivity{
+    private FloatingActionButton mFab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +23,29 @@ public class ToolBarActivity extends AppCompatActivity{
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.zhihu_toolbar_menu);
-
         toolbar.setNavigationIcon(R.mipmap.ic_drawer_home);
-
         toolbar.setTitle(R.string.home_page);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+        mFab = (FloatingActionButton) findViewById(R.id.fb);
+//        mFab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar.make(v,"FAB",Snackbar.LENGTH_LONG).setAction("cancel", new View.OnClickListener(){
+//                    @Override
+//                    public void onClick(View v) {
+//                        Toast.makeText(ToolBarActivity.this,"消失了吧",Toast.LENGTH_SHORT).show();
+//                    }
+//                }).show();
+//            }
+//        });
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
