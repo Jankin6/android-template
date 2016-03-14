3ae65713a7f9f37f9766ec0be0c8b3fd3621f950
wangjianjun <jianjunwang1012@foxmail.com>
2016-03-15 00:31:11 +0800

    .

--- app/src/main/java/com/wangjj/toolbarapplication/ToolBarActivity.java
+++ app/src/main/java/com/wangjj/toolbarapplication/ToolBarActivity.java
@@ -2,7 +2,11 @@ package com.wangjj.toolbarapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,40 +16,61 @@ import android.widget.Toast;
 /**
  * Created by wangjj on 2016/3/11.
  */
-public class ToolBarActivity extends AppCompatActivity{
+public class ToolBarActivity extends AppCompatActivity {
     private FloatingActionButton mFab;
+    //将ToolBar与TabLayout结合放入AppBarLayout
+    private Toolbar mToolbar;
+    //DrawerLayout中的左侧菜单控件
+    private NavigationView mNavigationView;
+    //DrawerLayout控件
+    private DrawerLayout mDrawerLayout;
+    //Tab菜单，主界面上面的tab切换菜单
+    private TabLayout mTabLayout;
+    //v4中的ViewPager控件
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_toolbar);
 
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.inflateMenu(R.menu.zhihu_toolbar_menu);
-        toolbar.setNavigationIcon(R.mipmap.ic_drawer_home);
-        toolbar.setTitle(R.string.home_page);
-        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        //初始化ToolBar
+//        setSupportActionBar(mToolbar);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setHomeAsUpIndicator(android.R.drawable.ic_dialog_alert);
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mToolbar.inflateMenu(R.menu.zhihu_toolbar_menu);
+        mToolbar.setNavigationIcon(R.mipmap.ic_drawer_home);
+        mToolbar.setTitle(R.string.home_page);
+        mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
 
         mFab = (FloatingActionButton) findViewById(R.id.fb);
-//        mFab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Snackbar.make(v,"FAB",Snackbar.LENGTH_LONG).setAction("cancel", new View.OnClickListener(){
-//                    @Override
-//                    public void onClick(View v) {
-//                        Toast.makeText(ToolBarActivity.this,"消失了吧",Toast.LENGTH_SHORT).show();
-//                    }
-//                }).show();
-//            }
-//        });
-
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Snackbar.make(v, "FAB", Snackbar.LENGTH_LONG).setAction("cancel", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ToolBarActivity.this, "消失了吧", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
             }
         });
+
+//        mFab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
